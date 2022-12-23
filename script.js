579f58862e6357552864483acd358f631d1112fa
CryptoFallen <richardtom1203@protonmail.com>
2022-12-23 12:41:13 +0100

    Refactored and trimmed down repeatable code, DRY

--- script.js
+++ script.js
@@ -6,27 +6,28 @@ const image2 = document.getElementById("image2");
 const image3 = document.getElementById("image3");
 const textBox = document.getElementById("text-box");
 
+// Dark or Light Images - Using Template strings to cut down on lines of code
+function imageMode(color) {
+  image1.src = `img/undraw_proud_coder_${color}.svg`;
+  image2.src = `img/undraw_feeling_proud_${color}.svg`;
+  image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+}
+
 // Dark Mode Styles
 function darkMode() {
   nav.style.backgroundColor = "rgb(0 0 0 /50%)";
   textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
   toogleIcon.children[0].textContent = "Dark Mode"; //.textContent allows to change text securely
-  toogleIcon.children[1].classList.remove("fa-sun"); //removes sun icon and replaces with moon icon
-  toogleIcon.children[1].classList.add("fa-moon");
-  image1.src = "img/undraw_proud_coder_dark.svg"; //changing image
-  image2.src = "img/undraw_feeling_proud_dark.svg";
-  image3.src = "img/undraw_conceptual_idea_dark.svg";
+  toogleIcon.children[1].classList.replace("fa-sun", "fa-moon"); //replaces sun icon and with moon icon
+  imageMode("dark");
 }
 // Dark Mode Styles
 function lightMode() {
   nav.style.backgroundColor = "rgb(255 255 255 / 50%)";
   textBox.style.backgroundColor = "rgb(0 0 0 /50%)";
   toogleIcon.children[0].textContent = "Light Mode"; //.textContent allows to change text securely
-  toogleIcon.children[1].classList.remove("fa-moon");
-  toogleIcon.children[1].classList.add("fa-sun"); //removes sun icon and replaces with moon icon
-  image1.src = "img/undraw_proud_coder_light.svg"; //changing image
-  image2.src = "img/undraw_feeling_proud_light.svg";
-  image3.src = "img/undraw_conceptual_idea_light.svg";
+  toogleIcon.children[1].classList.replace("fa-moon", "fa-sun"); //replaces moon icon and with sun icon
+  imageMode("light");
 }
 // Switch These Dynamically
 function switchTheme(event) {
